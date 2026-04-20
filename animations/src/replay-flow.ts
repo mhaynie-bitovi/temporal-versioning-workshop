5691da4fe5d8d7cb3c5460636a96d4c11d06f038
Michael Haynie <mhaynie@bitovi.com>
2026-04-20 09:45:44 -0700

    remove animations

--- animations/src/replay-flow.ts
+++
@@ -1,8 +0,0 @@
-import { makeProject } from "@motion-canvas/core";
-
-import replayFlow from "./scenes/replay-flow/replay-flow?scene";
-
-export default makeProject({
-  name: "replay-flow",
-  scenes: [replayFlow],
-});
--- animations/src/replay-nde.ts
+++
@@ -1,8 +0,0 @@
-import { makeProject } from "@motion-canvas/core";
-
-import replayNDE from "./scenes/replay-nde/replay-nde?scene";
-
-export default makeProject({
-  name: "replay-nde",
-  scenes: [replayNDE],
-});
